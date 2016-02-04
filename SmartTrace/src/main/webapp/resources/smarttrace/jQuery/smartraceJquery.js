569400eb7c05dad3169a64087b6fe7250652e078
ignacio.m.larreta <ignacio.m.larreta@accenture.com>
2016-02-04 17:17:33 -0300

    Mejoras varias, faltan correjir algunos problemas con el jquery es posible que el menu de costado deje de funcionar en la proxima subida lo arreglo

--- SmartTrace/src/main/webapp/resources/smarttrace/jQuery/smartraceJquery.js
+++ SmartTrace/src/main/webapp/resources/smarttrace/jQuery/smartraceJquery.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function($) {
+	setIcons();
+});
+
+function setIcons(){
 	$(".ui-icon.ui-icon-seek-prev").text("");
 	$(".ui-icon.ui-icon-seek-next").text("");
 	$(".ui-icon.ui-icon-seek-end").text("");
 	$(".ui-icon.ui-icon-seek-first").text("");
 	$("span.ui-icon.ui-icon-circle-triangle-e").text("");
 	$("span.ui-icon.ui-icon-circle-triangle-w").text("");
-});
+}
