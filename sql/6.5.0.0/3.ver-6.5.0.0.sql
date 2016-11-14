43be41c5ce773102f8a7d68acdfa8923e7b6a373
jgonzmol <jgonzmol@BUE-858029A-12.usersad.everis.int>
2016-11-14 13:55:30 -0300

    Se realiza el cambio para registrar la fecha de atentión, cuando se atiende y finaliza una WO desde HSP .

--- sql/6.5.0.0/3.ver-6.5.0.0.sql
+++ sql/6.5.0.0/3.ver-6.5.0.0.sql
@@ -50,6 +50,13 @@ if (v_count >= 1) then
 	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice CREW_MODIFICATIONS_IDX01');
 end if;	
 	
+select count(*) into v_count from all_indexes WHERE index_name = 'CREW_MODIFICATIONS_IDX02' AND owner = '{{schema}}';
+	
+if (v_count >= 1) then 
+
+	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice CREW_MODIFICATIONS_IDX02');
+end if;
+
 --TABLA PARA REPORTE DE WORK ORDERS ATENDIDAS
 select count(*) into v_count from all_tables where table_name = 'WORK_ORDER_CREW_ATTENTIONS' AND owner = '{{schema}}';
 
@@ -82,6 +89,17 @@ if (v_count >= 1) then
 	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice WO_CREW_ATTENTIONS_IDX01');
 end if;
 
+select count(*) into v_count from all_indexes WHERE index_name = 'WO_CREW_ATTENTIONS_IDX02' AND owner = '{{schema}}';
+
+if (v_count >= 1) then 
+	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice WO_CREW_ATTENTIONS_IDX02');
+end if;
+
+select count(*) into v_count from all_indexes WHERE index_name = 'WO_CREW_ATTENTIONS_IDX03' AND owner = '{{schema}}';
+
+if (v_count >= 1) then 
+	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice WO_CREW_ATTENTIONS_IDX03');
+end if;
 
 select count(*) into v_count from all_indexes WHERE index_name = 'WAREHOUSE_ELEMENTS_IDX13' AND owner = '{{schema}}';
 
@@ -89,5 +107,23 @@ if (v_count >= 1) then
 	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice WAREHOUSE_ELEMENTS_IDX13');
 end if;
 	
+select count(*) into v_count from all_indexes WHERE index_name = 'UPLOAD_FILE_PARAM_IDX1' AND owner = '{{schema}}';
+
+if (v_count >= 1) then 
+	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice UPLOAD_FILE_PARAM_IDX1');
+end if;
+
+select count(*) into v_count from all_indexes WHERE index_name = 'FILE_DETAIL_PROCESSES_IDX1' AND owner = '{{schema}}';
+
+if (v_count >= 1) then 
+	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice FILE_DETAIL_PROCESSES_IDX1');
+end if;
+
+select count(*) into v_count from all_indexes WHERE index_name = 'ADJUSTMENT_ELEMENTS_IDX02' AND owner = '{{schema}}';
+
+if (v_count >= 1) then 
+	DBMS_OUTPUT.Put_line ('Se creo correctamente el indice ADJUSTMENT_ELEMENTS_IDX02');
+end if;
+
 end;
 /
