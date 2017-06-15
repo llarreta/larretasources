634ba9fd63030db54999a7bc00c67234dc736d7a
Leonel Larreta <leonel@larreta.com.ar>
2017-06-15 17:29:41 -0300

    Problemas de instanciacion de la clase con spring

--- SchoolApi/src/main/java/ar/com/larreta/school/messages/ObligationData.java
+++ SchoolApi/src/main/java/ar/com/larreta/school/messages/ObligationData.java
@@ -5,12 +5,28 @@ import java.util.Date;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import ar.com.larreta.rest.messages.JSONable;
 import ar.com.larreta.rest.messages.JSONableCollection;
-import ar.com.larreta.rest.messages.ParametricData;
 import ar.com.larreta.validators.annotations.NotNull;
 
 @Component @Scope("prototype")
-public class ObligationData extends ParametricData {
+public class ObligationData extends JSONable {
+	
+	private Long 			id;
+	private String 			description;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	
 	@NotNull(message="productGroup.required")
 	private Long 	productGroup;
